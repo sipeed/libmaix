fe0e156d47d16693c92ac9e2dd887898a48db9bf
junhuanchen <junhuanchen@qq.com>
2021-10-16 16:20:06 +0800

    [update] image rgb888 to bgr888

--- components/libmaix/lib/arch/r329/src/libmaix_image/src/libmaix_image.cpp
+++ components/libmaix/lib/arch/r329/src/libmaix_image/src/libmaix_image.cpp
@@ -1,6 +1,7 @@
 
 #include "libmaix_image.h"
 #include "libmaix_debug.h"
+#include <cstdlib>
 
 // inline static unsigned short make16color(unsigned char r, unsigned char g, unsigned char b)
 // {
@@ -12,10 +13,55 @@
 
 libmaix_err_t libmaix_image_soft_convert(struct libmaix_image *obj, libmaix_image_mode_t mode, struct libmaix_image **new_img)
 {
-  switch (mode)
+
+	libmaix_err_t err = LIBMAIX_ERR_NONE;
+	if(new_img == NULL)
+	{
+		return LIBMAIX_ERR_PARAM;
+	}
+	if(mode == obj->mode)
+	{
+		return LIBMAIX_ERR_NONE;
+	}
+	if(obj->width==0 || obj->height==0 || obj->data == NULL)
+	{
+		return LIBMAIX_ERR_PARAM;
+	}
+
+  int ret = 0, new_mem = 0;
+  if((*new_img) == NULL)
   {
-      case LIBMAIX_IMAGE_MODE_RGB565:
-        if (obj->mode == LIBMAIX_IMAGE_MODE_RGB888) {
+    *new_img = libmaix_image_create(obj->width, obj->height, obj->mode, obj->layout, NULL, true);
+    if(!(*new_img))
+    {
+      return LIBMAIX_ERR_NO_MEM;
+    }
+    new_mem = 1;
+  }
+  else
+  {
+    if( (*new_img)->data == NULL)	
+    {
+      (*new_img)->data = malloc(obj->width * obj->height * 3);
+      if(!(*new_img)->data)
+      {
+        return LIBMAIX_ERR_NO_MEM;
+      }
+      (*new_img)->is_data_alloc = true;
+      new_mem = 2;
+    }
+    (*new_img)->layout = obj->layout;
+    (*new_img)->width = obj->width;
+    (*new_img)->height = obj->height;
+  }
+
+  // -------------------------------
+  switch(obj->mode)
+  {
+    case LIBMAIX_IMAGE_MODE_RGB888: {
+      switch (mode)
+      {
+        case LIBMAIX_IMAGE_MODE_RGB565: {
           if (obj == *new_img || obj->width != (*new_img)->width || obj->height != (*new_img)->height) return LIBMAIX_ERR_PARAM;
           uint8_t *rgb888 = (uint8_t *)obj->data;
           uint16_t *rgb565 = (uint16_t *)(*new_img)->data;
@@ -23,17 +69,43 @@ libmaix_err_t libmaix_image_soft_convert(struct libmaix_image *obj, libmaix_imag
             // *rgb565 = make16color(rgb888[0], rgb888[1], rgb888[2]);
             *rgb565 = ((((rgb888[0] >> 3) & 31) << 11) | (((rgb888[1] >> 2) & 63) << 5) | ((rgb888[2] >> 3) & 31));
           }
-          return LIBMAIX_ERR_NONE;
-        } else {
-          ;
+          (*new_img)->mode = mode;
+          break;
         }
-        break;
-      
-      default:
-        break;
+        case LIBMAIX_IMAGE_MODE_BGR888: {
+          // printf("libmaix_image_hal_convert obj->mode %d mode %d \r\n", obj->mode, mode);
+          uint8_t *rgb = (uint8_t *)(obj->data), *bgr = (uint8_t *)(*new_img)->data;
+          for (uint8_t *end = rgb + obj->width * obj->height * 3; rgb < end; rgb += 3, bgr += 3) {
+            bgr[2] = rgb[0], bgr[1] = rgb[1], bgr[0] = rgb[2];
+          }
+          (*new_img)->mode = mode;
+          break;
+        }
+        default:
+          err = LIBMAIX_ERR_PARAM;
+          break;
+      }
+      break;
+    }
+    default:
+      err = LIBMAIX_ERR_NOT_IMPLEMENT;
+      break;
   }
-
-  return LIBMAIX_ERR_NOT_IMPLEMENT;
+  // -------------------------------
+  
+  if(err != LIBMAIX_ERR_NONE)
+  {
+    if(new_mem == 2)
+    {
+      free((*new_img)->data);
+      (*new_img)->data = NULL;
+    }
+    else if (new_mem == 1)
+    {
+      libmaix_image_destroy(new_img);
+    }
+  }
+	return err;
 }
 
 libmaix_err_t libmaix_image_soft_resize(struct libmaix_image *obj, int w, int h, struct libmaix_image **new_img)
